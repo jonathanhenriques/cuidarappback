506c086470ccbc9c623793a7645964eb2c675992
Jonathan Henrique <silva.henriquejonas@gmail.com>
2023-07-16 14:14:52 -0300

    1.1

--- src/main/resources/db/migration/prod/V0002__adicionando_situacao_exame.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE tb_exame add situacao tinyint(1) DEFAULT 1 not null;
-UPDATE tb_exame set situacao = true;
--- src/main/resources/db/migration/prod/V0003__adicionando_coluna_codigo_uuid_paciente.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE tb_paciente add codigo VARCHAR(36) not null after id;
-UPDATE tb_paciente set codigo = uuid();
-alter table tb_paciente add constraint uk_pedido_codigo unique (codigo);
