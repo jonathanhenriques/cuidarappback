bd2ec4fb176b7bbf449979218b67c552a7041fa2
Jonathan Henrique <silva.henriquejonas@gmail.com>
2023-07-16 00:31:04 -0300

    tentando consertar deploy railway - resolvendo loop railway - 1.8

--- src/main/java/com/cuidar/configuration/AppConfigExterno.java
+++ src/main/java/com/cuidar/configuration/AppConfigExterno.java
@@ -7,19 +7,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@Slf4j
-@Configuration
-@PropertySource(value = "file:C:/Users/DevJonathan/Desktop/external1.properties", ignoreResourceNotFound = true)
+//@Slf4j
+//@Configuration
+//@PropertySource(value = "file:C:/Users/DevJonathan/Desktop/external1.properties", ignoreResourceNotFound = true)
 //@ComponentScan(basePackages = "seu.pacote.de.componentes")
 public class AppConfigExterno {
 
-//    @Value("${perfil.escolhido:app.prop}")
-    @Value("${nome.perfil}")
-    private String perfilEscolhido;
-
-    @Bean
-    public void AppConfigBean(){
-        log.info("PERFIL ATIVO: {}", perfilEscolhido);
-    }
+////    @Value("${perfil.escolhido:app.prop}")
+//    @Value("${nome.perfil}")
+//    private String perfilEscolhido;
+//
+//    @Bean
+//    public void AppConfigBean(){
+//        log.info("PERFIL ATIVO: {}", perfilEscolhido);
+//    }
 
 }
