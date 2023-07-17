0c1d23370d81f6195d42a0df566244408c3766f6
Jonathan Henrique <silva.henriquejonas@gmail.com>
2023-07-17 17:14:20 -0300

    1.12 -  profile prod (testando BD variaveis servidor prod) - railway java 17

--- src/main/resources/db/prod/V0003__adicionando_coluna_codigo_uuid_paciente.sql
+++ src/main/resources/db/prod/V0003__adicionando_coluna_codigo_uuid_paciente.sql
@@ -1,2 +1,3 @@
-ALTER TABLE tb_paciente ADD COLUMN codigo VARCHAR(36) NOT NULL DEFAULT uuid_generate_v4() AFTER id;
+ALTER TABLE tb_paciente ADD COLUMN codigo VARCHAR(36) NOT NULL DEFAULT uuid_generate_v4();
 ALTER TABLE tb_paciente ADD CONSTRAINT uk_paciente_codigo UNIQUE (codigo);
+
